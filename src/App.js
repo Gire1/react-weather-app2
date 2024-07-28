cf45ee07abb2d421b651434eabaacc2c9d26c7d6
Gire1 <168452060+Gire1@users.noreply.github.com>
2024-07-28 21:47:47 +0200

    Added bootstrap

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultcity="Lisbon" />
+        <footer>
+          This project was coded by{""}
+          <a
+            href="https://github.com/Gire1/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Grace Chifamba
+          </a>{" "}
+          and is{""}
+          <a
+            href="https://github.com/Gire1/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
